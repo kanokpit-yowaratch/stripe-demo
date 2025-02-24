4a39d7a2d15516347c58168c145aab636394f407
Kanokpit <thana.drnn@hotmail.com>
2025-02-24 20:55:49 +0700

    fix: add types

---
+++ src/types/common.ts
@@ -0,0 +1,13 @@
+export type ShippingAddress = {
+    fullName: string;
+    address: string;
+    phone: string;
+    email: string;
+}
+
+export const intitialShippingAddress = {
+    fullName: "",
+    address: "",
+    phone: "",
+    email: ""
+}
